3e9580298280684073cc691748269b6df903f9b5
manuelaj2705-tech <manuelaj.27.05@gmail.com>
2026-02-26 07:18:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ st.write ("facilmente puedo realizar backend y frontemd")
 image = Image.open ("imagenmultimodales.jpg")
 
 st.image (image,caption= "interfaces multimodales")
+
+
+texto = st.text_imput ("Escribir algo" , "caricatura")
+st.write ("El texto escrito es ",texto)
+
+st.subheader ("ahora usamos dos columnas")
