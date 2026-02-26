84da4d87cf0b013d25899863f87b031166b70780
manuelaj2705-tech <manuelaj.27.05@gmail.com>
2026-02-26 07:25:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ st.write ("El texto escrito es ",texto)
 
 st.subheader ("ahora usamos dos columnas")
 
-col1,col2,col3 =st.columnas(3)
+col1,col2 =st.columnas(2)
 
-with col1;
+with col1:
     st.subheader("Esta es la primera columna")
     st.write ("las interfaces multimodales mejoran la experiencia de usuario ")
     resp= st.checkbox ("estoy de acuerdo")
