5b71174c9bce9fc21f49c413fb0ac28db4832db5
manuelaj2705-tech <manuelaj.27.05@gmail.com>
2026-02-26 07:12:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from PIL import image 
+from PIL import Image 
 
 st.title ("snoppy")
 
 st.header ("En este espacio comienzso a desarrolar mis apps ")
 st.write ("facilmente puedo realizar backend y frontemd")
-image = image.open ("imagenmultimodales.jpg")
+image = Image.open ("imagenmultimodales.jpg")
 
 st.image (image,caption= "interfaces multimodales")
