dd0336d6b38e3cb0d663ffb0c9dc628c1b0e5e69
manuelaj2705-tech <manuelaj.27.05@gmail.com>
2026-02-26 07:23:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,16 @@ image = Image.open ("imagenmultimodales.jpg")
 st.image (image,caption= "interfaces multimodales")
 
 
-texto = st.text_imput ("Escribir algo" , "caricatura")
+texto = st.text_imput ("Escribir algo" , "Este es mi texto")
 st.write ("El texto escrito es ",texto)
 
 st.subheader ("ahora usamos dos columnas")
+
+col1,col2,col3 =st.columnas(3)
+
+with col1;
+    st.subheader("Esta es la primera columna")
+    st.write ("las interfaces multimodales mejoran la experiencia de usuario ")
+    resp= st.checkbox ("estoy de acuerdo")
+    if resp:
+      st.write("correcto")
